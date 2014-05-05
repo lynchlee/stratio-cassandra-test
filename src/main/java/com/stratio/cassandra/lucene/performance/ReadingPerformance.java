d2ab15804cad8111618700d341f9d3d02fadd18f
Óscar Puertas <opuertas@paradigmatecnologico.com>
2014-05-05 17:15:00 +0200

    Changing number of threads

--- src/main/java/com/stratio/cassandra/lucene/performance/ReadingPerformance.java
+++ src/main/java/com/stratio/cassandra/lucene/performance/ReadingPerformance.java
@@ -5,35 +5,40 @@ import java.util.concurrent.Executors;
 
 public class ReadingPerformance {
 
-	private final static int NUMBER_OF_THREADS = 8;
-	private final static int NUMBER_OF_TASKS = 100;
+    private final static int NUMBER_OF_THREADS = 24;
+    private final static int NUMBER_OF_TASKS = 100;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ExecutorService executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+        ExecutorService executor = Executors
+                .newFixedThreadPool(NUMBER_OF_THREADS);
 
-		long start = System.currentTimeMillis();
+        long start = System.currentTimeMillis();
 
-		for (int i = 0; i < NUMBER_OF_TASKS; i++) {
+        for (int i = 0; i < NUMBER_OF_TASKS; i++) {
 
-			Runnable worker = new MyRunnable("Thread-" + i);
-			executor.execute(worker);
-		}
-		executor.shutdown();
-		while (!executor.isTerminated()) {
-		}
+            Runnable worker = new MyRunnable("Thread-" + i);
+            executor.execute(worker);
+        }
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+        }
 
-		long time = System.currentTimeMillis() - start;
-		long numQueries = MyRunnable.numQueries;
+        long time = System.currentTimeMillis() - start;
+        long numQueries = MyRunnable.numQueries;
 
-		System.out.println("TOTAL TIME " + time);
-		System.out.println("NUM EXECUTED TASKS " + numQueries);
-		System.out.println("AVERAGE QUERY LATENCY " + ((double) MyRunnable.totalQueryTime / (double) numQueries) + "ms");
-		System.out.println("AVERAGE QUERY TIME " + ((double) time / (double) numQueries) + "ms");
-		System.out.println("QUERIES PER SECOND " + ((double) numQueries / (((double) time) / 1000D)));
+        System.out.println("TOTAL TIME " + time);
+        System.out.println("NUM EXECUTED TASKS " + numQueries);
+        System.out.println("AVERAGE QUERY LATENCY "
+                + ((double) MyRunnable.totalQueryTime / (double) numQueries)
+                + "ms");
+        System.out.println("AVERAGE QUERY TIME "
+                + ((double) time / (double) numQueries) + "ms");
+        System.out.println("QUERIES PER SECOND "
+                + ((double) numQueries / (((double) time) / 1000D)));
 
-		executor.shutdownNow();
+        executor.shutdownNow();
 
         System.exit(1);
-	}
+    }
 }
